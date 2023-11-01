fdd400c9844b1081709ba3f43d6719f4e0c0014d
LucioSandoval <lucio.scaceres@gmail.com>
2023-11-01 13:59:20 -0300

    portafolio 2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PortafolioII.Models;
+
+namespace PortafolioII.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpGet]
+    [Route("/projects")]
+    public IActionResult Projects()
+    {
+        return View("Projects");
+    }
+
+    [HttpGet]
+    [Route("/contact")]
+    public IActionResult Contact()
+    {
+        return View("Contact");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PortafolioII.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
